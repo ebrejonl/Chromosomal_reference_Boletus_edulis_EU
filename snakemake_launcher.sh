6cd04ef35604a313e97ca4aea39e0413e5b8fa38
ebrejonl <etienne.brejon@hotmail.fr>
2024-11-15 11:07:16 +0100

    added laptop launcher

--- snakemake_launcher.sh
+++ snakemake_launcher.sh
@@ -1,5 +1,4 @@
 ## script to launch the pipeline on the cluster
-
 snakemake --jobs 50 \
   -p \
   --default-resources mem_mb=50000 threads=1 \
@@ -20,3 +19,15 @@ snakemake --jobs 50 \
       -o JOBS_err/{name}.{jobid}.out \
       --mem={resources.mem_mb}' \
       --jn Ref.{name}.{jobid}.sh #--dry-run
+
+
+## script for laptop
+snakemake --jobs 50 \
+  -p \
+  --default-resources mem_mb=12 threads=1 \
+  --use-singularity \
+  --use-conda \
+  --rerun-triggers mtime \
+  --latency-wait 1000 \
+  --rerun-incomplete \
+  --keep-going  #--dry-run
