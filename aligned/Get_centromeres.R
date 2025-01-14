dc4b356bbc37ad327b7ff4cef980873185307457
ebrejonl <etienne.brejon@hotmail.fr>
2025-01-14 14:49:25 +0100

    t

--- aligned/Get_centromeres.R
+++ aligned/Get_centromeres.R
@@ -37,8 +37,12 @@ all_hic_data |> filter(log_counts>10) |>
   ggplot(aes(x=y, y=log_counts ))+
   geom_point()+ 
   facet_wrap(~chromosome1)
+
+
+
+
 library(zoo)
-p=all_hic_data |> filter(log_counts>10) |> 
+p=all_hic_data |> filter(log_counts>10) |> ## keeping only the highest values
   group_by(chromosome1) |> 
   mutate(roll_c=rollmean(log_counts, k = 40, align="center", na.pad = TRUE))
 
@@ -50,24 +54,79 @@ p |> ggplot()+
 # per chromosome
 p |> filter(chromosome1=="chr1") |> 
   ggplot()+ 
-  geom_point(aes(x = y, y = log_counts)) +
-  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
-  geom_vline(xintercept = 1000000)
+  geom_point(aes(x = y, y = log_counts), size=2.5) +
+#  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
+  geom_vline(xintercept = 1100000)
 
 p |> filter(chromosome1=="chr2") |> 
   ggplot()+ 
   geom_point(aes(x = y, y = log_counts)) +
-  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
+#  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
   geom_vline(xintercept = 1800000)
 
 p |> filter(chromosome1=="chr3") |> 
   ggplot()+ 
   geom_point(aes(x = y, y = log_counts)) +
-  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
+#  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
   geom_vline(xintercept = 2750000)
 
 p |> filter(chromosome1=="chr4") |> 
   ggplot()+ 
   geom_point(aes(x = y, y = log_counts)) +
+#  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
+  geom_vline(xintercept = 2200000)
+
+p |> filter(chromosome1=="chr5") |> 
+  ggplot()+ 
+  geom_point(aes(x = y, y = log_counts)) +
   geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
-  geom_vline(xintercept = 2750000)
+  geom_vline(xintercept = 2050000)
+
+p |> filter(chromosome1=="chr6") |> 
+  ggplot()+ 
+  geom_point(aes(x = y, y = log_counts)) +
+  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
+  geom_vline(xintercept = 1150000)
+
+p |> filter(chromosome1=="chr7") |> 
+  ggplot()+ 
+  geom_point(aes(x = y, y = log_counts)) +
+#  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
+  geom_vline(xintercept = 1350000)
+
+p |> filter(chromosome1=="chr8") |> 
+  ggplot()+ 
+  geom_point(aes(x = y, y = log_counts)) +
+  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
+  geom_vline(xintercept = 1400000)
+
+p |> filter(chromosome1=="chr9") |> 
+  ggplot()+ 
+  geom_point(aes(x = y, y = log_counts)) +
+  geom_line(aes(x = y,y = roll_c), color ='red', linewidth=1.5) +
+  geom_vline(xintercept = 1250000)
+
+p |> filter(chromosome1=="chr10") |> 
+  ggplot()+ 
+  geom_point(aes(x = y, y = log_counts)) +
+  geom_vline(xintercept = 2400000)
+
+p |> filter(chromosome1=="chr11") |> 
+  ggplot()+ 
+  geom_point(aes(x = y, y = log_counts)) +
+  geom_vline(xintercept = 1600000)
+
+ql=unique(p$chromosome1)  |> as.data.frame()
+ql$centromere=c(1100000,1800000,2750000,2200000,2050000,1150000,1350000,1400000,1250000,2400000,1600000)
+colnames(ql) <- c("chromosome1", "Centromere")
+
+p =  p   |> left_join(ql)
+supp_centromeres=p |> 
+  ggplot(aes(x=y/1000000, y=log_counts ))+
+  geom_point()+ xlab("Position (Mbp)")+ylab("Hi-C interaction >10")+
+  geom_segment(aes(x=Centromere/1000000, y=-Inf, xend =Centromere/1000000, yend = Inf), color="red")+
+  facet_wrap(~chromosome1, scales = "fixed", ncol=5)
+
+supp_centromeres
+
+ggsave(supp_centromeres, file = "Supplementary_figure_centromeres.pdf", width = 12, height = 8)
