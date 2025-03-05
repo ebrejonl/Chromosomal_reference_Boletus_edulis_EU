fa2bbb7fff8cfe86debb68c51cad45b58590f2cb
ebrejonl <etienne.brejon@hotmail.fr>
2025-03-05 16:26:01 +0100

    t

--- rules/Rscripts/Coverage.R
+++ rules/Rscripts/Coverage.R
@@ -50,25 +50,28 @@ Coverage |> filter(!str_detect(CHROM, "Chr")) |> summarize(m=mean(meancov,na.rm=
 library(ggrastr)
 library(ggpubr)
 
-pppp <- ggplot()+
+ppp_legend <- ggplot()+
   geom_rect(data=fai, mapping=aes(xmin=g_start, xmax=g_end, ymin=-Inf,ymax=Inf, 
                                          fill=factor(as.character(even_odd))), color="transparent") + 
   scale_fill_manual(values=c(`0`="white", `1`="#cacfd2", `2`="transparent"),guide="none")+
-  scale_x_continuous(labels = function(x){sprintf("%.1fMbp",x*1e-6)}, 
+  scale_x_continuous(labels = function(x){sprintf("%.0f",x*1e-6)}, 
                      sec.axis = sec_axis(trans = identity, 
                                          breaks=fai$g_mid[1:11],
                                          # labels=str_c("scf_",1:11)
-                                         labels=fai$CHROM[1:11]))+
+                                         labels=fai$CHROM[1:11]), expand = c(0, 0))+
   theme(axis.text.x.top = element_text(vjust = 0.5))+
+    new_scale_fill() +
     #angle=90, vjust = 0.5))+
   rasterize(
     geom_ribbon(Coverage, mapping = aes(x=POS_absolute, 
       ymin = meancov-sdcov, 
-      ymax = meancov+sdcov), fill="#526374ff", alpha=.8))+
-    geom_line(Coverage, mapping=aes(x=POS_absolute, y=meancov), 
-              color="#f8c26eff",linewidth=0.6) +
-    theme_bw()+ xlab("Genomic position")+ ylab("Depth of coverage") +
+      ymax = meancov+sdcov,fill="Standart\n deviation"), alpha=.8))+
+      geom_line(data = Coverage, aes(x = POS_absolute, y = meancov, color = "Mean"), linewidth = 0.3) +
+      # Custom color and fill legends
+      scale_color_manual(name = "Coverage", values = c("Mean" = "#f8c471")) +
+      scale_fill_manual(name = "", values = c("Standart\n deviation" = "#34495e")) +
+    theme_bw()+ xlab("Genomic position (Mb)")+ ylab("Depth of coverage") +
     ylim(-5,60)+ geom_bracket(xmin = 41000000, xmax = 50000000, y.position = 50,
           vjust = -0.5,  label.size = 3.5,tip.length = 0.025, label="Unplaced scaffolds")
 
-saveRDS(pppp, snakemake@output[['Coverage']])
+saveRDS(ppp_legend, snakemake@output[['Coverage']])
--- rules/Rscripts/Karyotype_plot.R
+++ rules/Rscripts/Karyotype_plot.R
@@ -49,56 +49,148 @@ Telomeres=read_tsv("Results/Telomeres/TELO_telomeric_repeat_windows.tsv") |> ren
   rename(Start="window") |> mutate(End = Start + 10000) |> mutate(type="Telomere") |> 
   select(name, Start, End, type)
 
-fai_smaller=fai_deeper |> select(name,length,g_start,g_end,g_mid) |> distinct()
-Telomeres = Telomeres |> left_join(fai_smaller, by ="name") |> mutate(idx=NA, even_odd=NA, Caz=NA, Effector=NA)
+# MAT loci
+# 
+MAT <- data.frame(Start=c(953944,3072277 ), End=c(980720,3085748), g_end=c(32057100,4111618), g_start=c(29387091, 0),# add chr data here before merge
+            g_mid=c(30722096,2055809)  ,   type=c("MATa", "MATb"), name=c("Chr9","Chr1"))
+
+# Centromeres
+Centromeres=data.frame(name=paste0("Chr", rep(1:11)), 
+                       Start=c(1100000,1800000,2750000,2200000,
+                               2050000,1150000,1350000,1400000,1250000,2400000,1600000), 
+                       End=c(1100000,1800000,2750000,2200000,
+                                             2050000,1150000,1350000,1400000,1250000,2400000,1600000),
+                       type="Centromere")
+
+Telomeres=rbind(Telomeres, Centromeres)
 
 
 first_merge <- merge(Cazymes, Effectors,  by=c("name", "Start", "End", "type"), all = T) 
 fai_deeper <- left_join(Hap_2_indec, first_merge, by="name") %>% filter(!name=="NA") 
+
+fai_smaller=fai_deeper |> select(name,length,g_start,g_end,g_mid) |> distinct()
+Telomeres = Telomeres |> left_join(fai_smaller, by ="name") |> mutate(idx=NA, even_odd=NA, Caz=NA, Effector=NA)
+
+missing_columns <- setdiff(names(fai_deeper), names(MAT))
+MAT[missing_columns] <- NA
+MAT <- MAT[names(fai_deeper)]
+
 fai_deeper <- rbind(MAT, fai_deeper)
 fai_deeper$type <- factor(fai_deeper$type, levels = c("CAZymes", "Effector", "MATa", "MATb"))
 fai_deeper_eff <- fai_deeper %>% filter(type=="Effector")
 fai_deeperM <- fai_deeper 
-#fai_deeper <- fai_deeper %>% filter(!type=="MATa", !type=="MATb")
 order=paste0("Chr", 1:11)
 fai_deeper = fai_deeper |> mutate(name=fct_relevel(name, order))
 fai_deeper = rbind(fai_deeper, Telomeres[names(fai_deeper)]) 
-library(ggchicklet)
-library(ggrepel)
-library(MetBrewer)
-library(data.table)
 
-#met.brewer(n=11, "Johnson")
-karyo <-fai_deeper |> mutate(name=fct_relevel(name, order)) |> 
-ggplot(., mapping=aes(ymin = g_start-g_start+1, ymax = g_end-g_start+1, 
-                               xmin = 0, xmax = 0.2))+#, col=as.factor(type)))+#,col="black", fill="#85929e") +
-  ggchicklet:::geom_rrect(#col="black",
-                          fill="#e9eaea", cex=0.01,##cccccc", 
-                          r = unit(0.4, 'npc')) +
-  geom_segment(fai_deeper |> filter(!type=="MATa", !type=="MATb", !type=="Telomere") ,
-               mapping = aes(x = 0.1, xend = 0.1, y = Start-5000, yend = End+5000, color=type ), #color=type),
-               linewidth=12)+
-  xlab(NULL)+ylab(NULL)+
-  scale_color_manual(aesthetics = "color", values =c("#03286b","#ae0000") ,
-                     labels=c( "CAZymes", "Effector\nProtein", "MATa", "MATb"))+ 
-  geom_segment(fai_deeper_eff, mapping = aes(x = 0.1, xend = 0.1, y = Start-1000, yend = End+1000),  color="#ae0000",
-               linewidth=12)+
-  geom_label_repel(fai_deeper |> filter(type=="MATb" | type=="MATa"), mapping = aes(x = 0.1, y = End, label=type),  color="#86a995",
-              size=3.3 , fontface="bold")+
-  geom_point(fai_deeper %>% filter(type=="Telomere" & Start>230000), mapping = aes(x  = 0.1, y  = Start),shape=24, fill="yellow",color="brown", size=4)+
-  geom_point(fai_deeper %>% filter(type=="Telomere" & Start<250000), mapping = aes(x  = 0.1, y  = Start),shape=25, fill="yellow",color="brown", size=4)+
-  theme_light()+
-  facet_wrap(~as.numeric(substr(name,4,5)), nrow = 1, strip.position = "bottom") + # ncol=6
-   # annotate("text", x=3, y=200000, label="}")+
-  theme(panel.grid = element_blank(), axis.text.x = element_blank(), 
-        axis.text.y = element_blank(), panel.spacing = unit(1.5, "cm"), panel.border = element_blank(),
-        axis.ticks.x = element_blank() , panel.grid.minor = element_blank(),
-        strip.text = element_text(size = 20, color = "black", hjust = 0.14), axis.ticks.y.left = element_blank(),
-        # strip.background = element_rect(fill="#ecf0f3"),
-        strip.background = element_blank(), 
-        legend.title = element_blank(),
-        legend.key.size = unit(1,"line"),
-        legend.key.height = unit(3,"line"),
-        legend.text = element_text(size=20))  #;karyo
+fai_deeper |> head()
+
+
+### The features first 
+data_features <-  fai_deeper # already feature format ?
+
+centro_length <- 150000
+chr_width <- .6
+clr <- "#E5E5E5"
+#clr_h <- "gray60"
+clr_h <- "gray50"
+
+
+karyo2= fai_deeper |> 
+  filter(type=="Centromere") |> 
+  mutate( centro_pos=Start,
+          centro_start=Start - .5 *centro_length,
+          centro_end=Start +.5 * centro_length) |> 
+  mutate(name=as.factor(name))
+
+
+karyo_poly2 <- karyo2 |>
+  mutate(
+    x = map(as.numeric(name), \(x, w = chr_width * .5) {
+      rep(x + w * c(1, -1, -1, 1, 1), 2)
+    }),
+    y = map2(length, centro_pos, \(l, C, cl = centro_length * .5) {
+      c(0, 0, C - cl, C - cl, 0, C + cl, C + cl, l, l, C + cl)
+    }),
+    y_sharp = map2(length, centro_pos, \(l, C, cl = centro_length * .5) {
+      lw = (C - cl) * .5
+      up = C + (l - C + cl) * .5
+      c(lw, lw, C - cl, C - cl, lw, C + cl, C + cl, up, up, C + cl)
+    }),
+    part = list(rep(c("lower", "upper"), each = 5))
+  ) |>
+  unnest(cols = c(x, y, y_sharp, part))|> as.data.frame()
+
+
+karyo_hour2 <- karyo2 |>
+  mutate(
+    x = map(as.numeric(name), \(x, w = chr_width * .5) {
+      x + w * c(1, -1, 1, -1, 1)
+    }),
+    y = map2(length, centro_pos, \(l, C, cl = centro_length * .5) {
+      c(C - cl, C - cl, C + cl, C + cl, C - cl)
+    })
+  ) |>
+  unnest(cols = c(x, y)) |> as.data.frame()
+
+karyo_hour2
+
+Feature=c("CAZymes",    "Effector")
+
+
+karyo2.0=karyo2 |>
+  ggplot(aes(x = name)) +
+  geom_polygon(
+    data = karyo_poly2,
+    aes(x = x, y = y_sharp, group = str_c(name, part)),
+    fill = clr
+  ) +
+  geom_shape(
+    data = karyo_poly2,
+    aes(x = x, y = y, group = str_c(name, part)),
+    r = unit(20, "pt"),
+    fill = clr
+  ) +
+  geom_polygon(
+    data = karyo_hour2,
+    aes(x = x, y = y, group = name),
+    fill = clr_h
+  )+
+  geom_linerange(
+    data = fai_deeper |> filter(type %in% Feature),
+    inherit.aes = FALSE,
+    aes(
+      xmin = as.numeric(name) - chr_width * .41,
+      xmax = as.numeric(name) + chr_width * .41,
+      y = Start,
+      color = type
+    ),
+    linewidth = 1
+  )  +    
+  scale_color_manual(aesthetics = "color", values =c("#ff4c4c","#3777FF"))+
+
+  geom_label(fai_deeper |> filter(type=="MATb" | type=="MATa"), 
+      inherit.aes = FALSE,
+      mapping = aes(x=as.numeric(name), y = End, label=type),  
+      color="#86a995", size=3.5 , fontface="bold", position = "identity")+
+
+  geom_point(fai_deeper %>% filter(type=="Telomere" & Start>230000), 
+  mapping = aes(x  = as.numeric(name), y  = Start),
+  shape=24, fill="yellow",color="brown", size=4)+
+
+  geom_point(fai_deeper %>% filter(type=="Telomere" & Start<250000), 
+  mapping = aes(x  = as.numeric(name), y  = Start),
+  shape=25, fill="yellow",color="brown", size=4)+
+
+  scale_x_continuous(
+    breaks = as.numeric(unique(fai_deeper$name)),
+    labels = as.character(substr(unique(fai_deeper$name), 4, 5))
+  ) +
+      
+    theme_minimal()+ xlab(NULL)+ylab("Genomic position")+ theme(axis.title.y.left = element_text(color="black", size=17, vjust=2.5),
+  axis.text.y.left = element_text(color="black", size=15),
+panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+axis.text.x.bottom = element_text(size=18, color="black",face = "plain"),
+legend.title = element_blank(), legend.text = element_text(size=18))#;karyo
 
 ggsave("snakemake@output[['Karyo_plot']]", karyo, height = 9, width =14)
