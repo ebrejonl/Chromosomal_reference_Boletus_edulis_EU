ebce6664bf76660d7a35e95c6cb3cabc976fe964
ebrejonl <etienne.brejon@hotmail.fr>
2025-03-05 16:31:35 +0100

    t

--- aligned/plotHIC.R
+++ aligned/plotHIC.R
@@ -1,4 +1,3 @@
-#remotes::install_github("aidenlab/straw/R")
 library(strawr)
 library(tidyverse)
 library(ggrastr)
@@ -29,7 +28,6 @@ for (chr1 in chromosomes) {
 all_hic_data$chromosome1 <- factor(all_hic_data$chromosome1, levels = paste0("chr", 1:11))
 all_hic_data$chromosome2 <- factor(all_hic_data$chromosome2, levels = rev(paste0("chr", 1:11)))
 # 
-# Add log-transformed counts column (log10)
 all_hic_data <- all_hic_data %>%
 mutate(log_counts = log2(counts + 1)) |> 
     filter(as.numeric(str_sub(chromosome1,4,5)) <= as.numeric(str_sub(chromosome2,4,5)) )
@@ -37,13 +35,10 @@ mutate(log_counts = log2(counts + 1)) |>
 
 hi_c_plot=ggplot(all_hic_data, aes(x = x, y = y, fill = log_counts)) +
   geom_tile() +
-  facet_grid(chromosome2 ~ chromosome1,space = "free", scales = "free")+#, scales = "free") +  # Facet by chromosome pairs
-  scale_fill_gradient2(low = "#ebf5fb", mid = "#e74c3c",   high = "black", midpoint = 10) + # "#fef9e7" # choose colors
+  facet_grid(chromosome2 ~ chromosome1,space = "free", scales = "free")+#, scales = "free") +  
+  scale_fill_gradient2(low = "#ebf5fb", mid = "#e74c3c",   high = "black", midpoint = 10) +
   theme_void() +
   labs(fill = "Interaction (log2)") + theme(panel.spacing = unit(0, "lines"))
 
-ggsave("hi_c_plot2.pdf", hi_c_plot, width=8, height = 6)
-
-
-#ggsave("hi_c_plot_small.pdf", hi_c_plot, width=6, height = 5)
+ggsave("hi_c_plot.pdf", hi_c_plot, width=8, height = 6)
 
